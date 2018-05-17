24bea70ec91f24f99d0ba7c16a01cdd12bdf853e
Usman <usman@lightcurve.io>
2018-05-17 12:58:42 +0200

    Add symlink for tmux conf

--- symlink_config_files.sh
+++ symlink_config_files.sh
@@ -3,3 +3,4 @@ ln -fs ~/dot-files/.tern-config ~/.tern-config
 ln -fs ~/dot-files/.vimrc ~/.vimrc
 ln -fs ~/dot-files/.eslintrc.json  ~/.eslintrc.json
 ln -fs ~/dot-files/.zshrc  ~/.zshrc
+ln -fs ~/dot-files/.tmux.conf  ~/.tmux.conf
